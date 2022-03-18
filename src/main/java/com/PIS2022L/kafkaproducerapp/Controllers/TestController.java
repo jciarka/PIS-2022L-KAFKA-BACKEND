a9d6e71cabdfee30679f723b257de6fb994be26c
Jakub Ciarka <01104656@pw.edu.pl>
2022-03-18 15:08:15 +0100

    Global cors configuration - allow all

--- src/main/java/com/PIS2022L/kafkaproducerapp/Controllers/TestController.java
+++ src/main/java/com/PIS2022L/kafkaproducerapp/Controllers/TestController.java
@@ -1,13 +1,15 @@
-package com.PIS2022L.kafkaproducerapp.controllers;
+package com.PIS2022L.kafkaproducerapp.Controllers;
 
 import com.PIS2022L.kafkaproducerapp.Models.Test.TestResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "api/test/")
 public class TestController {
     @GetMapping()

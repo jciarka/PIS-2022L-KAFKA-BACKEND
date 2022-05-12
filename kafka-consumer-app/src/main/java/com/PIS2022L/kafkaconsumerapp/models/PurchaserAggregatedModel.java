e90e6bd53ecf3438d557d547a440fa6c04e920a4
Jakub Ciarka <01104656@pw.edu.pl>
2022-05-13 01:47:28 +0200

    Procedures for mongo to fetch top clients by order and items count

---
+++ kafka-consumer-app/src/main/java/com/PIS2022L/kafkaconsumerapp/models/PurchaserAggregatedModel.java
@@ -0,0 +1,19 @@
+package com.PIS2022L.kafkaconsumerapp.models;
+
+import com.PIS2022L.kafkaconsumerapp.domain.MongoSelgrosItem;
+import com.PIS2022L.kafkaordermodels.domain.Address;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PurchaserAggregatedModel {
+    private long purchasersCode;
+    private long total;
+    private Address deliveryAddress;
+    private Long contactPhone;
+    private List<MongoSelgrosItem> items;
+}
+
